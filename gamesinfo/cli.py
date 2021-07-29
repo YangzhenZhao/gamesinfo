57092cf0c2c1675a432e79b737f15d811457e74d
zhaoyz <954241552@qq.com>
2021-07-29 17:15:38 +0800

    Add -m cmd

--- gamesinfo/cli.py
+++ gamesinfo/cli.py
@@ -3,9 +3,10 @@ import click
 from gamesinfo.olympic2020 import show_info
 
 
-# 130000:3271  14:00开始
 # 130000:3283  15:00开始
 # 130000:212  15:25开始
 @click.command()
-def cli():
-    show_info("130000:3271")
+@click.option("-m", "--mid")
+def cli(mid):
+    if mid is not None:
+        show_info(mid)
--- gamesinfo/olympic2020.py
+++ gamesinfo/olympic2020.py
@@ -1,10 +1,10 @@
 import json
+import time
 
 import requests
 from prettytable import PrettyTable
 
 from gamesinfo.const import CUSTOM_HEADER
-import time
 
 
 def show_info(mid):
@@ -20,13 +20,13 @@ def show_info(mid):
         info = json.loads(text)
         version = info["version"]
         if version == last_version:
-            time.sleep(30)
+            time.sleep(10)
         last_version = version
         if info["code"] != 0:
             print("暂无数据!")
             break
-        match_info = info["data"]['matchInfo']
-        print(match_info['matchDesc'])
+        match_info = info["data"]["matchInfo"]
+        print(match_info["matchDesc"])
         print(f"比赛状态: {match_info['period']}")
         print(f"预计开始时间: {match_info['startTime']}")
         print(f"预计结束时间: {match_info['endTime']}")
@@ -41,10 +41,6 @@ def show_info(mid):
                 table.add_row(row_info)
         print(table.get_string())
 
-        if match_info['period'] == "已结束":
+        if match_info["period"] == "已结束":
             break
-        time.sleep(30)
-
-
-if __name__ == "__main__":
-    show_info("130000:3271")
+        time.sleep(25)
